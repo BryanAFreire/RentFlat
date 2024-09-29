cbf4fda7f6f66bb97ab7099e2afe5a7f97a5153d
Bryan Freire <bfreire@krugerschool.edu.ec>
2024-09-29 14:50:34 -0500

    updated login getting data from localstorage

--- asserts/js/sesion-storage.js
+++ asserts/js/sesion-storage.js
@@ -1,17 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const startSession = () => {
-        const user = document.querySelector('#email');
-        const password = document.querySelector('#password');
-        if (user.value === mockEmail && password.value === mockPassword) {
-            const token = generateJWT(user);
+        const userEmail = document.querySelector('#email').value;
+        const password = document.querySelector('#password').value;
+
+        // Recover users from localstorage
+        const users = JSON.parse(localStorage.getItem('users')) || [];
+        
+        // Finds if user exist
+        const user = users.find(user => user.email === userEmail && user.password === password);
+
+        if (user) {
+            const token = generateJWT(user); 
             localStorage.setItem('token', token);
             window.location.href = '/new_flat.html';
         } else {
-            createErrorMessage(password, "Credentials are wrong. Please verify and try again.");
+            createErrorMessage(document.querySelector('#password'), "Credentials are wrong. Please verify and try again.");
         }
-    }
-    const mockEmail = 'bryan@gmail.com';
-    const mockPassword = 'password123';
+    };
+
     const btnLogIn = document.querySelector('#btn_log_in');
-    btnLogIn.addEventListener('click', startSession)
+    btnLogIn.addEventListener('click', startSession);
 });
