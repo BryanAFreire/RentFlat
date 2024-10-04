8cad69546bf6de6d169cfb89ee771b252fcc5242
Bryan Freire <bfreire@krugerschool.edu.ec>
2024-10-03 23:18:08 -0500

    updated new flats

--- asserts/js/new-flats-events.js
+++ asserts/js/new-flats-events.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     userGreeting();
+
+    const listProfile = (event) => {
+        if (contentProfile.style.display === 'inline-block') {
+            contentProfile.style.display = 'none';
+        } else {
+            contentProfile.style.display = 'inline-block';
+        }
+
+    }
+    const profileIco = document.querySelector('.content-profile');
+    const contentProfile = document.querySelector('.menu-profile');
+    if (profileIco) {
+        profileIco.addEventListener('click', listProfile)
+    }
+    
     const city = document.querySelector('#city');
     city.addEventListener('keyup', () => { (city.value = '') ? createErrorMessage(city, "Please enter a city.") : deleteErrorMessage(city); })
     console.info('halo!')
